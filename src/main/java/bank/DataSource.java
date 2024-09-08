e34eaa7e50cd0c976ceaf1b2ebddd0d2aa513b43
Loredana10 <124152490+Loredana10@users.noreply.github.com>
2024-09-08 15:57:09 +0100

    Working in the DataSource class, creating a connection with the database using SQL, constructing a customer from data source

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -2,6 +2,8 @@ package bank;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 //class to connect to the database, as well as reading and writing from it
@@ -22,7 +24,31 @@ public class DataSource {
     
   }
 
+  public static Customer getCustomer(String username){
+    //query to get username from database
+    //question mark serves as a place holder
+    String sql = "select * from customers where username = ?";
+    Customer customer = null;
+
+    //try with resources to connect to the database
+    try(Connection connection = connect(); PreparedStatement statement = connection.prepareStatement(sql)){
+      statement.setString(1, username);
+      try(ResultSet resultSet = statement.executeQuery()){
+        customer = new Customer(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getString("username"), resultSet.getString("password"), resultSet.getInt("account_id"));
+      }
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    return customer;
+  }
+
   public static void main(String[] args){
-    connect();
+    //main class
+    //retrieve a customers details and printing out their name using getName()
+    Customer customer = getCustomer("twest8o@friendfeed.com");
+
+    System.out.println(customer.getName());
   }
 }
